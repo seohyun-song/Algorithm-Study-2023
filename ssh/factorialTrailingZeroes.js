a3c4ed2242b19d8789e01083c55d134ebf9401e5
seohyun song <seohyun@seohyunui-MacBookPro.local>
2023-06-01 19:38:54 +0900

    fix: leetCode/172/recursion/medium 리팩토링

--- ssh/factorialTrailingZeroes.js
+++ ssh/factorialTrailingZeroes.js
@@ -40,3 +40,26 @@ function getFactorial(n) {
 	if (n <= 1) return 1n;
 	return n * getFactorial(n - 1n);
 }
+
+/**
+ * 리팩토링
+ * 10의 배수이면 traniling zero 생김
+ * 1!~4! -> trailing zero = 0
+ * 5!~9! -> trailing zero = 1 / 5! = 5*4*3*2*1 (5*2=10) 5! 부터 10의 배수 생김
+ * 10! ~ 14! -> trailing zero = 2 / 10! = 10*9! -> 100의 배수되므로
+ * 15! ~ 19! -> trailing zero = 3 / 15! = 15*14! -> 15=5*3 여기서 5와 짝수를 곱하면 10을 만들수 있음 -> 1000의 배수
+ * ...
+ * 25! -> trailing zero = 6 / 25! = 25*24! -> 25 = 5*5이므로 5가 2개나 있으므로 짝수와 곱해서 100을 만들수 있음
+ *
+ * n<5보다 작으면 traniling zero = 0
+ * n>=5크면 trailing zero = n/5
+ * 하지만 예외 케이스 n/5 >= 5이면 trailing zero가 n/5 아님
+ */
+var trailingZeroes = function (n) {
+	let answer = 0;
+	while (n >= 5) {
+		answer += Math.floor(n / 5);
+		n = Math.floor(n / 5);
+	}
+	return answer;
+};
