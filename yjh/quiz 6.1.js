75555552391cfc50c5808efa7fad89b57d27f5bd
[SW5] 유종훈 <soojhyoung@gmail.com>
2023-06-01 19:20:24 +0900

    문제풀이 최종

--- yjh/quiz 6.1.js
+++ yjh/quiz 6.1.js
@@ -2,11 +2,23 @@
  * @param {number} n
  * @return {number}
  */
-var trailingZeroes = function(n) {
+
+function factorial(n) {
+  if (n === 0 || n === 1) {
+    return num = 1;
+  }
+  return num = n * factorial(n - 1);
+}
+
+function trailingZeroes(num) {
   let count = 0;
-  while (n > 0) {
-      n = Math.floor(n / 5);
-      count += n;
+  const numStr = String(num);
+  for (let i = numStr.length - 1; i >= 0; i--) {
+    if (numStr[i] === '0') {
+      count++;
+    } else {
+      break;
+    }
   }
   return count;
-};
+}
